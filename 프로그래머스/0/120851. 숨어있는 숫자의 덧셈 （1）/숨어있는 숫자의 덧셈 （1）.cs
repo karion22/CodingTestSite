d6f8856d0af267de38d03ed2321cea7b336353a6
karion22 <kadas22@naver.com>
2024-10-03 14:27:10 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.38 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public int solution(string my_string) {
+        int answer = 0;
+        
+            foreach(char c in my_string)
+            {
+                if (char.IsDigit(c))
+                    answer += (c - '0');
+            }
+
+        
+        return answer;
+    }
+}
