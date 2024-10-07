05fa080e94bd82bbce35809fe504e2a0372c49bb
karion22 <kadas22@naver.com>
2024-10-07 12:55:08 +0900

    [level 0] Title: 순서쌍의 개수, Time: 1.26 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int solution(int n) {
+        
+            List<int> res = new List<int>();
+
+            int left = 1, right = n;
+
+            if (left == right)
+                res.Add(left);
+            else
+            {
+                while (left < right)
+                {
+                    if (n % left == 0)
+                    {
+                        res.Add(left);
+
+                        int div = n / left;
+                        if (div != left)
+                        {
+                            res.Add(div);
+                            right = div;
+                        }
+                    }
+
+                    left++;
+                }
+            }
+
+            return res.Count;
+    }
+}
